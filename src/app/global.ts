8680ad19d8719037dd9d6281db512d856e836fa6
= <=>
2019-03-03 19:40:37 +0530

    upload done

--- src/app/global.ts
+++ src/app/global.ts
@@ -4,5 +4,8 @@ export const GlobalVariable = Object.freeze({
     LOGIN : 'http://192.168.0.100:8080/api/login'
     // BASE_API_URL: 'http://10.10.12.87:8080/api/',
     // LOGIN : 'http://10.10.12.87:8080/login'
+    BASE_API_URL: '/api/',
+    LOGIN : '/api/login'
+    
    
 });
